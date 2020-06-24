2fc7298dcef36b105e4791ec58dfdff033bc62af
xiaolu sun <xiaolusun@xiaolus-MBP.fios-router.home>
2020-06-23 23:07:52 -0400

    add content

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contact(){
+ return(<div>
+     <button>
+         <a href={"https://github.com/xiaolu119"}>
+<i class="fab fa-github"></i></a>
+</button>
+ </div>
+ )
+
+}
+
+export default Contact;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Portfolio(){
+ return(<div>
+     <button>
+         <a href={"https://github.com/xiaolu119"}>
+<i class="fab fa-github"></i></a>
+</button>
+ </div>
+ )
+
+}
+
+export default Portfolio;
